f6c114768dff6101848ef242da157c15ce6b26a0
Yoana Zelova <29174828+yzelova@users.noreply.github.com>
2018-09-12 15:30:31 +0300

    add new resources and improvements

--- app/routers/index.js
+++ app/routers/index.js
@@ -6,7 +6,15 @@ const BookDbHelper = require('../../helpers/db/book');
 
 router.get('/', async function(request, response){
     const books = await BookDbHelper.getAllBooks();
-    response.render('index', {books});
+    const data = [];
+    for(const book of books) {
+        data.push({
+            book: book.book,
+            user: book.user
+        })
+    }
+    console.log(data)
+    response.render('index');
 }) 
 
 
